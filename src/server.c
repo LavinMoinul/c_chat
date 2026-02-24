e8494e74d6680a975cade5d40f5e9342a9dfa408
Lavin Moinul <lavinm@Lavins-MacBook-Pro.local>
2026-02-24 14:34:42 -0600

    feat: add basic tcp server that binds and listens

---
+++ src/server.c
@@ -0,0 +1,86 @@
+#include <arpa/inet.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+static int parse_port(const char *s) {
+    char *end = NULL;
+    long val = strtol(s, &end, 10);
+    if (end == s || *end != '\0') return -1;
+    if (val < 1 || val > 65535) return -1;
+    return (int)val;
+}
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        fprintf(stderr, "usage: %s <port>\n", argv[0]);
+        return 1;
+    }
+
+    int port = parse_port(argv[1]);
+    if (port < 0) {
+        fprintf(stderr, "bad port: %s\n", argv[1]);
+        return 1;
+    }
+
+    int listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (listen_fd < 0) {
+        perror("socket");
+        return 1;
+    }
+
+    int yes = 1;
+    if (setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) < 0) {
+        perror("setsockopt");
+        close(listen_fd);
+        return 1;
+    }
+
+    struct sockaddr_in addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    addr.sin_port = htons((uint16_t)port);
+
+    if (bind(listen_fd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+        perror("bind");
+        close(listen_fd);
+        return 1;
+    }
+
+    if (listen(listen_fd, 16) < 0) {
+        perror("listen");
+        close(listen_fd);
+        return 1;
+    }
+
+    printf("server listening on port %d\n", port);
+    printf("test: nc 127.0.0.1 %d\n", port);
+
+    while (1) {
+        struct sockaddr_in client_addr;
+        socklen_t client_len = sizeof(client_addr);
+
+        int client_fd = accept(listen_fd, (struct sockaddr *)&client_addr, &client_len);
+        if (client_fd < 0) {
+            if (errno == EINTR) continue;
+            perror("accept");
+            break;
+        }
+
+        char ip[INET_ADDRSTRLEN];
+        const char *ip_str = inet_ntop(AF_INET, &client_addr.sin_addr, ip, sizeof(ip));
+        if (!ip_str) ip_str = "unknown";
+
+        printf("client connected from %s:%d\n", ip_str, ntohs(client_addr.sin_port));
+
+        close(client_fd);
+    }
+
+    close(listen_fd);
+    return 0;
+}
